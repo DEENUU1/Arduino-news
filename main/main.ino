3dd36fe20b0751b12f4ce8ee168cfe23cda61fce
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-20 12:42:41 +0200

    Script to display messages from Serial Monitor and scroll them on the screen

---
+++ main/main.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
+const int padding = 0;
+const int lcdWidth = 16;
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String dataFromComputer = Serial.readString();
+    lcd.clear();
+
+    int textLength = dataFromComputer.length();
+    if (textLength <= lcdWidth) {
+      
+      lcd.setCursor(padding, 0);
+      lcd.print(dataFromComputer);
+    } else {
+      for (int i = 0; i <= textLength - lcdWidth; i++) {
+        lcd.setCursor(padding, 0);
+        lcd.print(dataFromComputer.substring(i, i + lcdWidth));
+        delay(500); 
+      }
+    }
+    
+    delay(3000);
+    lcd.clear();
+  }
+}
