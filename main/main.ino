f10701f295b3812668b39f2080ab9bc42bea72dc
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-21 14:44:41 +0200

    Add configuration for temperature sensore and display current temp on LCD

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,11 @@
 #include <LiquidCrystal.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+# define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
 const int padding = 0;
@@ -18,6 +25,14 @@ void scrollText(int textLength, String text) {
 }
 
 void loop() {
+
+  lcd.setCursor(0, 1);
+  sensors.requestTemperatures();
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.setCursor(6, 1);
+  lcd.print(("st. C"));
+  delay(1000);
+
   String welcomeMessage = "Welcome to news dashboard!";
   int welcomeMessageLenght = welcomeMessage.length();
 
@@ -34,4 +49,7 @@ void loop() {
       scrollText(textLength, dataFromComputer);
     }
   }
+
+
+
 }
